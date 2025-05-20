b5d84db52d2fe1f73440eb077397828e4c2a6878
ammarAlzaqam <git@ammaralzaqam.com>
2025-05-20 10:08:57 +0300

    add /api to API_DOMAIN

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const ARTICLE_PER_PAGE = 6;
-export const API_DOMAIN = "https://cloud-hosting-w-next-ts-tailwindcss.vercel.app";
+export const API_DOMAIN = "https://cloud-hosting-w-next-ts-tailwindcss.vercel.app/api";
