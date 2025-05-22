abf9c300daf174a9bb9607916b32de55d89c5c92
ammarAlzaqam <git@ammaralzaqam.com>
2025-05-22 09:53:35 +0300

    change mongodb connection

--- src/libs/mongoose.ts
+++ src/libs/mongoose.ts
@@ -1,37 +1,16 @@
-import mongoose, { Connection } from "mongoose";
+import mongoose from "mongoose";
 
-interface DBConnect {
-  conn: Connection | null;
-  promise: Promise<Connection> | null;
-}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-// تعريف عالمي
-declare global {
-  var mongoose: DBConnect | undefined;
-}
-
-// نحط الكاش هنا ونضمن إنه مش undefined
-const globalWithMongoose = global as typeof globalThis & {
-  mongoose?: DBConnect;
-};
-
-// نعمل التحقق هنا ونضمن إن cached مش undefined
-const cached: DBConnect = globalWithMongoose.mongoose ?? {
-  conn: null,
-  promise: null,
-};
-
-globalWithMongoose.mongoose = cached;
-
-export default async function connectDB(): Promise<Connection> {
+export default async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(process.env.MONGODB_URL as string)
-      .then((mongoose) => mongoose.connection);
+      .connect(process.env.MONGODB_URL!)
+      .then((c) => c.connection);
   }
-
   cached.conn = await cached.promise;
+  (global as any).mongoose = cached.conn;
   return cached.conn;
 }
