37fffc4a9a123df5cbe8a4ee1988c111ebc62fc0
ammarAlzaqam <git@ammaralzaqam.com>
2025-05-22 09:18:42 +0300

    connectDB settings

--- src/libs/mongoose.ts
+++ src/libs/mongoose.ts
@@ -1,21 +1,37 @@
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
-declare global {
-  var mongoose: any;
+interface DBConnect {
+  conn: Connection | null;
+  promise: Promise<Connection> | null;
 }
 
-const cached = global.mongoose || { conn: null, promise: null };
+// تعريف عالمي
+declare global {
+  var mongoose: DBConnect | undefined;
+}
 
-export default async function connectDB() {
+// نحط الكاش هنا ونضمن إنه مش undefined
+const globalWithMongoose = global as typeof globalThis & {
+  mongoose?: DBConnect;
+};
+
+// نعمل التحقق هنا ونضمن إن cached مش undefined
+const cached: DBConnect = globalWithMongoose.mongoose ?? {
+  conn: null,
+  promise: null,
+};
+
+globalWithMongoose.mongoose = cached;
+
+export default async function connectDB(): Promise<Connection> {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose
       .connect(process.env.MONGODB_URL as string)
-      .then((mongoose) => mongoose);
+      .then((mongoose) => mongoose.connection);
   }
 
   cached.conn = await cached.promise;
-  global.mongoose = cached;
   return cached.conn;
 }
